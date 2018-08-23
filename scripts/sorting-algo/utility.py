7299ca5668c9df531c7fa0eb466cc6bbf09283c7
wowotek <wowotek@wowotek-PC.168.1.7>
2018-08-23 15:38:15 +0700

    FIle Refactoring and minor code Refactoring

--- scripts/sorting-algo/utility.py
+++ scripts/sorting-algo/utility.py
@@ -1,22 +1,37 @@
-import os, sys
-def visualizer(unsorted_list:list, pointer_position:int, more_val="", wait=True, original_list:list=None) -> None:
+import os, sys, time
+from random import randint
+
+def visualizer(unsorted_list:list, pointer_position:int, more_val="", wait=0.032, original_list:list=None) -> None:
     os.system('clear')
+    sys.stdout.flush()
+    s = ""
     if(original_list is not None):
-        print(original_list)
-    print(unsorted_list)
+        s += str(original_list) + "\n"
+
+    s += str(unsorted_list) + "\n"
     for i in range(len(unsorted_list)):
-        for j in range(unsorted_list[i]):
-            print("-", end="")
+        s += "#" * unsorted_list[i]
 
-        for k in range((max(unsorted_list)-unsorted_list[i])+2):
-            print(" ", end="")
+        s += " " * int((max(unsorted_list) - unsorted_list[i]) + 2)
 
-        print(unsorted_list[i], end="")
+        s+=(str(unsorted_list[i]))
         if(i == pointer_position):
-            print("  <-- [" + str(pointer_position) + "]" + " " + str(more_val))
+            s += "  <-- [" + str(pointer_position) + "]" + " " + str(more_val) + "\n"
         else:
-            print()
-        sys.stdout.flush()
-    if(wait):
-        time.sleep(0.05)
+            s += "\n"
 
+    sys.stdout.flush()
+    sys.stdout.writelines(s)
+    time.sleep(wait)
+    os.system('clear')
+
+def generate_random_list(list_length:int=40, min_num:int=1, max_num:int=10, random_iteration:int=1000) -> list:
+    ret_list:list = []
+    random_number:int
+
+    for i in range(list_length):
+        for _ in range(random_iteration):
+            random_number = randint(min_num, max_num)
+        ret_list.append(random_number)
+
+    return ret_list
